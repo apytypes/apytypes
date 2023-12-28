000a7cc6bb5e2ce66dc14fa499a414f119a76dd0
Theodor Lindberg <theodor.lindberg@liu.se>
2023-12-28 14:00:15 +0100

    Add custom markers for pytest

--- test_apy_types/test_fixed_point.py
+++ test_apy_types/test_fixed_point.py
@@ -1,19 +1,13 @@
-import unittest
+import pytest
 import sys
 sys.path.append("../builddir/")
-from apy_types import APyFixed, STRING_TYPE
+from apy_types import APyFixed
 
 
-class TestFixedPoint(unittest.TestCase):
-    
-    def test_placeholder(self):
+def test_placeholder():
         # Not really a test, just a placeholder
         fx_a = APyFixed(8, 2, (1<<6) | (1 << 5))
         print(fx_a.to_string())
         print(fx_a)
         fx_a.increment_lsb()
         print(fx_a.to_string())
-        
-
-if __name__ == '__main__':
-    unittest.main()
--- test_apy_types/test_floating_point.py
+++ test_apy_types/test_floating_point.py
@@ -5,6 +5,7 @@ sys.path.append('../builddir/')
 from apy_types import APyFloat
 
 # Addition
+@pytest.mark.float_add
 @pytest.mark.parametrize(
     "test_in,test_exp", [("-APyFloat(5, 5, 2.75)", -2.75), 
                         ("-APyFloat(5, 5, -13.25)", 13.25)]
@@ -12,44 +13,52 @@ from apy_types import APyFloat
 def test_negation(test_in, test_exp):
     assert float(eval(test_in)) == test_exp
 
+@pytest.mark.float_add
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 2.75)", "APyFloat(5, 5, 2.5)"])))
-def test_addition_same_exp(lhs, rhs):
+def test_add_same_exp(lhs, rhs):
     # Add two numbers that have the same exponent
     assert float(eval(f"{lhs} + {rhs}")) == 5.25
 
+@pytest.mark.float_add
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 4)", "APyFloat(5, 5, 16)"])))
-def test_addition_diff_exp(lhs, rhs):
+def test_add_diff_exp(lhs, rhs):
     # Add two numbers that have different exponent
     assert float(eval(f"{lhs} + {rhs}")) == 20.0
 
+@pytest.mark.float_add
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 4, 12)", "APyFloat(5, 4, -4)"])))
-def test_addition_diff_sign(lhs, rhs):
+def test_add_diff_sign(lhs, rhs):
     # Add two numbers that have different sign
     assert float(eval(f"{lhs} + {rhs}")) == 8.0
 
+@pytest.mark.float_add
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 5.75)", "APyFloat(5, 5, 0.0)"])))
-def test_addition_zero(lhs, rhs):
+def test_add_zero(lhs, rhs):
     # Add two numbers that have different sign
     assert float(eval(f"{lhs} + {rhs}")) == 5.75
 
 
 # Subtraction
+@pytest.mark.float_sub
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 2.75)", "APyFloat(5, 5, 2.5)"])))
-def test_addition_same_exp2(lhs, rhs):
-    # Add two numbers that have the same exponent
+def test_sub_same_exp(lhs, rhs):
+    # Subtract two numbers that have the same exponent
     assert float(eval(f"{lhs} - {rhs}")) == (0.25 if float(eval(lhs)) == 2.75 else -0.25)
 
+@pytest.mark.float_sub
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 4)", "APyFloat(5, 5, 16)"])))
-def test_addition_diff_exp2(lhs, rhs):
-    # Add two numbers that have different exponent
+def test_sub_diff_exp(lhs, rhs):
+    # Subtract two numbers that have different exponent
     assert float(eval(f"{lhs} - {rhs}")) == (-12 if float(eval(lhs)) == 4 else 12)
 
+@pytest.mark.float_sub
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 4, 12)", "APyFloat(5, 4, -4)"])))
-def test_addition_diff_sign2(lhs, rhs):
-    # Add two numbers that have different sign
+def test_sub_diff_sign(lhs, rhs):
+    # Subtract two numbers that have different sign
     assert float(eval(f"{lhs} - {rhs}")) == (16.0 * (-1 if eval(lhs).is_sign_neg() else 1))
 
+@pytest.mark.float_sub
 @pytest.mark.parametrize("lhs,rhs", list(perm(["APyFloat(5, 5, 5.75)", "APyFloat(5, 5, 0.0)"])))
-def test_addition_zero2(lhs, rhs):
-    # Add two numbers that have different sign
+def test_sub_zero(lhs, rhs):
+    # Subtract two numbers that have different sign
     assert float(eval(f"{lhs} - {rhs}")) == (5.75 * (-1 if eval(lhs).is_zero() else 1))
