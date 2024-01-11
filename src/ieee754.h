cfa447009936bd1cec80621177551d41e4efb39e
Mikael Henriksson <mikael.henriksson@liu.se>
2024-01-11 15:29:19 +0100

    src: lower required C++ version to C++17

--- src/ieee754.h
+++ src/ieee754.h
@@ -1,6 +1,6 @@
 /*
  * C++-compatible bit fiddling with IEEE-754 floating-point numbers. Heavily inspired by
- * the GLIBC `ieee754.h`. Portable, but requires C++-20.
+ * the GLIBC `ieee754.h`. Portable and relies on C++17.
  *
  * Author: Mikael Henriksson (2024)
  */
@@ -9,10 +9,23 @@
 #define _CXX_IEEE754_FIDDLE
 
 #include "apy_util.h"
-#include <bit>      // std::endian
 #include <cstdint>  // uint64_t
 #include <cstring>  // std::memcpy
 #include <limits>   // std::numiric_limits
+
+// Test target machine native endianness in pure C++. Optimized away to single `true`
+// or `false`, depending on endianness, on all tested machines using most C++17
+// compatible compilers (godbolt.org):
+// * All GCC version supporting C++17
+// * All Clang version supporting C++17
+// * All versions of MSVC available at compiler explorer...
+static inline bool _MACHINE_IS_NATIVE_LITTLE_ENDIAN()
+{
+    uint32_t deadbeef = 0xDEADBEEF;
+    uint8_t arr[4];
+    std::memcpy(arr, &deadbeef, sizeof(deadbeef));
+    return arr[0] == uint8_t(0xEF) ? true : false;
+}
 
 static inline uint64_t type_pun_double_to_uint64_t(double d)
 {
@@ -28,7 +41,7 @@ static inline uint64_t type_pun_double_to_uint64_t(double d)
 static inline bool sign_of_double(double d)
 {
     uint64_t double_pun = type_pun_double_to_uint64_t(d);
-    if constexpr (std::endian::native == std::endian::little) {
+    if (_MACHINE_IS_NATIVE_LITTLE_ENDIAN()) {
          return bool(double_pun & (uint64_t(1) << 63));
     } else {  // std::endiand::native == std::endian::big
         return bool(double_pun & (uint64_t(1) << 0));
@@ -36,11 +49,11 @@ static inline bool sign_of_double(double d)
 }
 
 // Returns the *biased* exponent of a `double` in a `int64_t` 
-// Return value range: [0, 1023)
-static inline int64_t exp_of_double(double d)
+// Return value range: [0, 2048)
+static inline int64_t exponent_of_double(double d)
 {
     uint64_t double_pun = type_pun_double_to_uint64_t(d);
-    if constexpr (std::endian::native == std::endian::little) {
+    if (_MACHINE_IS_NATIVE_LITTLE_ENDIAN()) {
         return (double_pun << 1) >> 53;
     } else {  // std::endiand::native == std::endian::big
         throw NotImplementedException();
@@ -52,7 +65,7 @@ static inline int64_t exp_of_double(double d)
 static inline uint64_t mantissa_of_double(double d)
 {
     uint64_t double_pun = type_pun_double_to_uint64_t(d);
-    if constexpr (std::endian::native == std::endian::little) {
+    if (_MACHINE_IS_NATIVE_LITTLE_ENDIAN()) {
         return double_pun & 0x000FFFFFFFFFFFFF;
     } else {  // std::endiand::native == std::endian::big
         throw NotImplementedException();
