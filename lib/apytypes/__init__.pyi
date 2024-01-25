aaa2d4b55ad6f16ef71cba20a7e5abc92cf312ed
Mikael Henriksson <mikael.henriksson@liu.se>
2024-01-25 09:33:36 +0100

    Python stub generation for apytypes (#11)

---
+++ lib/apytypes/__init__.pyi
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from apytypes._apytypes import APyFixed
+from apytypes._apytypes import APyFixedOverflowMode
+from apytypes._apytypes import APyFixedRoundingMode
+from apytypes._apytypes import APyFloat
+from apytypes._apytypes import RoundingContext
+from apytypes._apytypes import RoundingMode
+from apytypes._apytypes import get_rounding_mode
+from apytypes._apytypes import set_rounding_mode
+from . import _apytypes
+
+__all__: list = [
+    "APyFloat",
+    "APyFixed",
+    "APyFixedRoundingMode",
+    "APyFixedOverflowMode",
+    "RoundingContext",
+    "RoundingMode",
+    "get_rounding_mode",
+    "set_rounding_mode",
+]
