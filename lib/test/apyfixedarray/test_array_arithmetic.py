2df2da09ef080510c3193aca3297f9c64497655e
Mikael Henriksson <mikael.henriksson@liu.se>
2024-01-29 11:01:01 +0100

    src: add APyFixedArray::__add__

--- lib/test/apyfixedarray/test_array_arithmetic.py
+++ lib/test/apyfixedarray/test_array_arithmetic.py
@@ -3,8 +3,15 @@ from apytypes import APyFixedArray
 import pytest
 
 
-def test_array_add():
+def test_array_add_raises():
     with pytest.raises(RuntimeError, match="APyFixedArray.__add__: shape missmatch"):
-        a = APyFixedArray([1, 2], bits=10, int_bits=5)
+        a = APyFixedArray([5, 6], bits=10, int_bits=5)
         b = APyFixedArray([1, 2, 3], bits=7, int_bits=2)
-        c = a + b
+        _ = a + b
+
+
+def test_array_add():
+    a = APyFixedArray([-5, -6, 7], bits=10, int_bits=5)
+    b = APyFixedArray([1, -2, 3], bits=7, int_bits=2)
+    c = a + b
+    assert c.is_identical(APyFixedArray([-4, -8, 10], bits=11, int_bits=6))
