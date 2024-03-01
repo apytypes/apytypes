c092b22ed0a7ac8ae2bb9318abe788d96b9dc7d2
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-03-01 09:31:26 +0100

    Do not skip __init__

--- docs/conf.py
+++ docs/conf.py
@@ -78,3 +78,14 @@ sphinx_gallery_conf = {
         "matplotlib",
     ),
 }
+
+
+# Do not skip __init__ of Python classes
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
