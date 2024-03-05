eabeca25a7621fd03d3b737e28497314c53de87f
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-03-05 09:34:15 +0100

    Update comparison and benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -17,9 +17,8 @@ CREATE_TWO_LONG_SCALARS = (
     "a = lib.create_scalar(0.37, 320, 160); b = lib.create_scalar(1.54, 320, 160)"
 )
 CREATE_TWO_ARRAYS = "import numpy as np; a = lib.create_array(np.random.rand(200, 200) - 0.5, 32, 16); b = lib.create_array(np.random.rand(200, 200), 32, 16)"
-CREATE_SHORT_VECTOR = (
-    "import numpy as np; a = lib.create_array(np.random.rand(20) - 0.5, 32, 16)"
-)
+CREATE_SCALAR_AND_ARRAY = "import numpy as np; a = lib.create_array(np.random.rand(200, 200) - 0.5, 32, 16); b = lib.create_scalar(-0.37854, 32, 16)"
+CREATE_SHORT_VECTOR = "import math;import numpy as np; a = lib.create_array(np.random.rand(20) - 0.5, 32, 16)"
 CREATE_VECTOR = (
     "import numpy as np; a = lib.create_array(np.random.rand(200) - 0.5, 32, 16)"
 )
@@ -30,14 +29,22 @@ benchmarks = {
     "Scalar division, 32-bit": ("c = a / b", CREATE_TWO_SCALARS),
     "Scalar abs, 32-bit": ("c = abs(a)", CREATE_TWO_SCALARS),
     "Scalar negation, 32-bit": ("c = -a", CREATE_TWO_SCALARS),
+    "Scalar right-shift, 32-bit": ("c = a >> 5", CREATE_TWO_SCALARS),
     "Scalar to float, 32-bit": ("c = float(a)", CREATE_TWO_SCALARS),
     "Scalar creation, 320-bit": ("lib.create_scalar(0.37, 320, 160)", "pass"),
     "Scalar addition, 320-bit": ("c = a + b", CREATE_TWO_LONG_SCALARS),
     "Scalar multiplication, 320-bit": ("c = a * b", CREATE_TWO_LONG_SCALARS),
     "Scalar division, 320-bit": ("c = a / b", CREATE_TWO_LONG_SCALARS),
+    "Scalar abs, 320-bit": ("c = abs(a)", CREATE_TWO_LONG_SCALARS),
+    "Scalar negation, 320-bit": ("c = -a", CREATE_TWO_LONG_SCALARS),
+    "Scalar right-shift, 320-bit": ("c = a >> 50", CREATE_TWO_LONG_SCALARS),
     "Scalar to float, 320-bit": ("c = float(a)", CREATE_TWO_LONG_SCALARS),
+    "Matrix-scalar addition": ("c = a + b", CREATE_SCALAR_AND_ARRAY),
+    "Matrix-scalar multiplication": ("c = a * b", CREATE_SCALAR_AND_ARRAY),
+    # "Matrix-scalar division": ("c = a / b", CREATE_SCALAR_AND_ARRAY),
     "20-element vector summation": ("sum(a)", CREATE_SHORT_VECTOR),
     "200-element vector summation": ("sum(a)", CREATE_VECTOR),
+    "20-element vector product": ("math.prod(a)", CREATE_SHORT_VECTOR),
     "Matrix creation": (
         "lib.create_array(a, 32, 16)",
         "import numpy as np; a = np.random.rand(200, 200)",
@@ -89,3 +96,25 @@ ax.set_xticks(
 )
 ax.legend(loc="upper left")
 fig.savefig("benchmark.png")
+
+# %%
+fig, ax = plt.subplots(layout="constrained", figsize=(7, 7))
+x = np.arange(len(benchmarks))  # the label locations
+width = 1 / (len(libraries) + 2)  # the width of the bars
+multiplier = 0
+
+for library, measurement in results.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=library)
+    multiplier += 1
+
+ax.set_yscale("log")
+ax.set_ylabel("Time, s")
+ax.set_xticks(
+    x + (len(libraries) - 1) * width / 2,
+    benchmarks.keys(),
+    rotation=90,
+)
+ax.legend(loc="upper left")
+
+fig.savefig("benchmark.pdf")
--- docs/comparison.rst
+++ docs/comparison.rst
@@ -32,6 +32,14 @@ Comparison matrix
       - No
       - No
       - No
+    * - Maximum wordlength [#wlfootnote]_
+      - None
+      - None
+      - None
+      - 64 [#numfifootnote]_
+      - None
+      - 53 [#pyfixpfootnote]_
+      - None
     * - Resizing of fixed-point results
       - Yes
       - Yes
@@ -71,6 +79,7 @@ Comparison matrix
 .. [#numfifootnote] ``numfi`` is limited to 64 total bits (including results of e.g. multiplication).
 .. [#pyfixpfootnote] ``pyfixp`` is essentially a package to simply quantize NumPy-arrays with doubles.
 .. [#fpbinaryfootnote] ``fpbinary`` provides a helper function to convert an array to a list with fixed-point numbers.
+.. [#wlfootnote] Note that this holds for intermediate results as well. For example, multiplying two 27-bit numbers will create a 54-bit result which will not give bit-accurate results with ``pyfixp``.
 
 .. list-table:: Floating-point libraries
     :header-rows: 1
@@ -85,6 +94,15 @@ Comparison matrix
     * - Custom floating-point formats
       - Yes
       - Yes
+    * - Binary representation of number [#binaryfootnote]
+      - Yes
+      - No
+    * - Maximum wordlength
+      - 32 bits for exponent, 64 bits for mantissa [#apytypesfootnote]
+      - None
+    * - Stochastic rounding
+      - Yes
+      - No
     * - Arrays
       - Yes
       - No
@@ -95,13 +113,17 @@ Comparison matrix
       - C++/Python
       - C/Python
 
+.. rubric:: Footnotes
+
+.. [#binaryfootnote] This means that it is possible to get a binary representation for comparison in e.g. hardware.
+.. [#numfifootnote] This is a design choice since ``apytypes``  is aimed at simulating shorter custom formats. However, there are unlimited word length support for fixed-point values which can be used for floating-point as well.
 
 
 Benchmark
 ---------
 
 This benchmark compares relevant fixed-point libraries from the list above.
-It is updated every documentation build and may vary over time.
+It is updated every documentation build and may vary over time as the code is further optimized.
 
 .. image:: _static/benchmark.png
     :alt: Benchmark numbers for different fixed-point libraries.
