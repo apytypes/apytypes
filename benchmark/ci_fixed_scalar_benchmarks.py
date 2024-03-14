8cb26a418e61f82a8962ffb3999e960269b37c07
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-03-14 23:54:22 +0100

    Fix stupid error

--- benchmark/ci_fixed_scalar_benchmarks.py
+++ benchmark/ci_fixed_scalar_benchmarks.py
@@ -4,63 +4,63 @@ import pytest
 
 
 def test_fixed_scalar_multiplication_short(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_multiplication_long(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_multiplication_mixed(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_division_short(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
 
     benchmark(lambda x, y: x / y, a, b)
 
 
 def test_fixed_scalar_division_long(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x / y, a, b)
 
 
 def test_fixed_scalar_division_mixed(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x / y, a, b)
 
 
 def test_fixed_scalar_addition_short(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_addition_long(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_addition_mixed(benchmark):
-    a = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 20, 1)
-    b = APyFixed.from_fixed(np.random.rand(1)[0] - 0.5, 200, 1)
+    a = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 20, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, 200, 1)
 
     benchmark(lambda x, y: x + y, a, b)
