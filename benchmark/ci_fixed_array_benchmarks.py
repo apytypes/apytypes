3645c23d83f768684f1de972d635f7bb935cf0b2
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-03-14 23:17:08 +0100

    Add floating-point array benchmarks

--- benchmark/ci_fixed_array_benchmarks.py
+++ benchmark/ci_fixed_array_benchmarks.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 14 08:46:11 2024
-
-@author: oscgu95
-"""
-
 from apytypes import APyFixedArray, APyFixed
 import numpy as np
 import pytest
@@ -79,28 +71,28 @@ def test_fixed_matrix_abs_200(benchmark):
 
 def test_fixed_scalar_matrix_addition_20(benchmark):
     a = APyFixedArray.from_float(np.random.rand(20, 20), 19, 1)
-    b = APyFixed.from_float(np.random.rand(1), 19, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0], 19, 1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_matrix_addition_200(benchmark):
     a = APyFixedArray.from_float(np.random.rand(200, 200), 19, 1)
-    b = APyFixed.from_float(np.random.rand(1), 19, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0], 19, 1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_20(benchmark):
     a = APyFixedArray.from_float(np.random.rand(20, 20), 19, 1)
-    b = APyFixed.from_float(np.random.rand(1), 19, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0], 19, 1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_200(benchmark):
     a = APyFixedArray.from_float(np.random.rand(200, 200), 19, 1)
-    b = APyFixed.from_float(np.random.rand(1), 19, 1)
+    b = APyFixed.from_float(np.random.rand(1)[0], 19, 1)
 
     benchmark(lambda x, y: x * y, a, b)
 
