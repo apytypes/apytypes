9b60280f232276da7485c4b4a4a4580aa77f6fd2
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 21:57:04 +0000

    add APyTypes Fixed-Point Array Benchmarks (pytest) benchmark result for 703317eb1539f6e42d6eb162d4b17b4c1ee7ce9e

---
+++ dev/bench/data.js
@@ -0,0 +1,137 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710453424214,
+  "repoUrl": "https://github.com/apytypes/apytypes",
+  "entries": {
+    "APyTypes Fixed-Point Array Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "oscar.gustafsson@gmail.com",
+            "name": "Oscar Gustafsson",
+            "username": "oscargus"
+          },
+          "committer": {
+            "email": "oscar.gustafsson@gmail.com",
+            "name": "Oscar Gustafsson",
+            "username": "oscargus"
+          },
+          "distinct": true,
+          "id": "703317eb1539f6e42d6eb162d4b17b4c1ee7ce9e",
+          "message": "Upgrade pip and try to keep previous data",
+          "timestamp": "2024-03-14T22:53:58+01:00",
+          "tree_id": "fec2a0bf5349e26201cae3349bd27a60e3fe09d2",
+          "url": "https://github.com/apytypes/apytypes/commit/703317eb1539f6e42d6eb162d4b17b4c1ee7ce9e"
+        },
+        "date": 1710453423738,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_multiplication_20",
+            "value": 24940.67339352915,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000015761666989121426",
+            "extra": "mean: 40.09514836353415 usec\nrounds: 24157"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_addition_20",
+            "value": 113183.81834255722,
+            "unit": "iter/sec",
+            "range": "stddev: 6.987248338757874e-7",
+            "extra": "mean: 8.835185229159203 usec\nrounds: 108838"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_addition_200",
+            "value": 1217.926807243039,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000025940494106710278",
+            "extra": "mean: 821.0674024522465 usec\nrounds: 1225"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_elementwise_multiplication_20",
+            "value": 1035366.4081494488,
+            "unit": "iter/sec",
+            "range": "stddev: 6.269326223710309e-8",
+            "extra": "mean: 965.8416499983913 nsec\nrounds: 103210"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_elementwise_multiplication_200",
+            "value": 17029.745173576255,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000024281802423835734",
+            "extra": "mean: 58.720784709780794 usec\nrounds: 17005"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_negation_20",
+            "value": 185341.27520298102,
+            "unit": "iter/sec",
+            "range": "stddev: 3.958669893878424e-7",
+            "extra": "mean: 5.395452248317735 usec\nrounds: 175439"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_negation_200",
+            "value": 2043.1423011541376,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000017473153261300007",
+            "extra": "mean: 489.4421692679538 usec\nrounds: 2050"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_abs_20",
+            "value": 190459.66950892645,
+            "unit": "iter/sec",
+            "range": "stddev: 4.0787932563897705e-7",
+            "extra": "mean: 5.250455398659254 usec\nrounds: 132732"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_abs_200",
+            "value": 2341.8803299657084,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000659536235025423",
+            "extra": "mean: 427.0073014425305 usec\nrounds: 2352"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_scalar_matrix_addition_20",
+            "value": 194549.48727320856,
+            "unit": "iter/sec",
+            "range": "stddev: 4.706289209648555e-7",
+            "extra": "mean: 5.140080367293314 usec\nrounds: 177274"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_scalar_matrix_addition_200",
+            "value": 2206.993847276767,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000190925738225806",
+            "extra": "mean: 453.10502393738454 usec\nrounds: 2214"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_scalar_matrix_multiplication_20",
+            "value": 221155.39141300844,
+            "unit": "iter/sec",
+            "range": "stddev: 2.8664438352596135e-7",
+            "extra": "mean: 4.521707536093916 usec\nrounds: 109446"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_scalar_matrix_multiplication_200",
+            "value": 2483.5651273181497,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000678700008382706",
+            "extra": "mean: 402.64698074571487 usec\nrounds: 2493"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_creation_20",
+            "value": 10557.225164636759,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000003107797844812452",
+            "extra": "mean: 94.7218596179678 usec\nrounds: 10450"
+          },
+          {
+            "name": "benchmark/ci_fixed_array_benchmarks.py::test_fixed_matrix_creation_200",
+            "value": 167.24399825770493,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006159659130290434",
+            "extra": "mean: 5.979287809533877 msec\nrounds: 168"
+          }
+        ]
+      }
+    ]
+  }
+}
