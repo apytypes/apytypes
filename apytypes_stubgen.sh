45c2fab3129be614c07892971cf092f82b2953ab
Mikael Henriksson <mikael.henriksson@liu.se>
2024-03-14 16:51:52 +0100

    nanobind: keep old stubs

--- apytypes_stubgen.sh
+++ apytypes_stubgen.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 #
-if ! command -v pybind11-stubgen; then
+if ! command -v stubgen; then
     echo "Failure: 'pybind11-stubgen' not in PATH"
     exit 1
 fi
 
+###
+### pybind11-stubgen
+###
 pybind11-stubgen -o lib apytypes                        \
     --enum-class-location "QuantizationMode:apytypes"   \
     --enum-class-location "OverflowMode:apytypes"
-
-# Hack the OverflowMode and QuantizationMode enums correct. Seems to a bug related to
-# pybind11-stubgen. Possibly related issues:
-# https://github.com/sizmailov/pybind11-stubgen/pull/196
-# https://github.com/sizmailov/pybind11-stubgen/issues/207
 sed -i 's/apytypes.QuantizationMode/QuantizationMode/g' lib/apytypes/_apytypes.pyi
 sed -i 's/apytypes.OverflowMode/OverflowMode/g' lib/apytypes/_apytypes.pyi
+
+###
+### MyPy stubgen
+###
+#stubgen -o lib -m apytypes
+#stubgen -o lib -m apytypes._apytypes
+
+###
+### Nanobind stubgen
+###
+#nanobind-stubgen --out lib apytypes
 
 # Format using black
 black lib/apytypes/*.pyi
