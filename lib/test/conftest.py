cec005abe84616226e74c5aa2a77534db571ab14
Mikael Henriksson <mikael.henriksson@liu.se>
2024-04-23 14:52:34 +0200

    ci: test on both NumPy v1.26.4 and v2.0.0

--- lib/test/conftest.py
+++ lib/test/conftest.py
@@ -4,4 +4,11 @@ import apytypes
 def pytest_collection_finish(session):
     print("")
     print("APyTypes test suite:")
-    print(apytypes._get_simd_version_str())
+    print(f" * {apytypes._get_simd_version_str()}")
+
+    try:
+        import numpy as np
+
+        print(f" * NumPy version: {np.__version__}")
+    except ImportError:
+        print(f" * No NumPy support detected")
