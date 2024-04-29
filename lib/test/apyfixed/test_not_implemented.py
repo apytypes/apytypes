0a97b7624e25cf3f34f466418cdf9d54467bbbc7
Mikael Henriksson <mikael.henriksson@liu.se>
2024-04-29 17:35:40 +0200

    src: test non-implemented fixed<->int arithmetic

--- lib/test/apyfixed/test_not_implemented.py
+++ lib/test/apyfixed/test_not_implemented.py
@@ -3,7 +3,7 @@ from apytypes import APyFixed
 import pytest
 
 
-def test_not_implemented_yet():
+def test_from_str():
     with pytest.raises(ValueError, match="Not implemented"):
         APyFixed.from_str("0", bits=1, int_bits=0, base=8)
     with pytest.raises(ValueError, match="Not implemented"):
@@ -11,9 +11,22 @@ def test_not_implemented_yet():
     with pytest.raises(ValueError, match="Not implemented"):
         APyFixed.from_str("0", bits=1, int_bits=0, base=-1)
 
+
+def test__str__():
     with pytest.raises(ValueError, match=r"APyFixed::to_string_oct"):
         APyFixed(0, 1, 0).__str__(base=8)
     with pytest.raises(ValueError, match=r"APyFixed::to_string_hex"):
         APyFixed(0, 1, 0).__str__(base=16)
     with pytest.raises(ValueError, match=r"APyFixed::to_string"):
         APyFixed(0, 1, 0).__str__(base=37)
+
+
+def test_missing_arithmetic():
+    with pytest.raises(ValueError, match=r"Not implemented"):
+        _ = APyFixed(0, 1, 0) + 4
+    with pytest.raises(ValueError, match=r"Not implemented"):
+        _ = APyFixed(0, 1, 0) - 4
+    with pytest.raises(ValueError, match=r"Not implemented"):
+        _ = APyFixed(0, 1, 0) * 4
+    with pytest.raises(ValueError, match=r"Not implemented"):
+        _ = APyFixed(0, 1, 0) / 4
