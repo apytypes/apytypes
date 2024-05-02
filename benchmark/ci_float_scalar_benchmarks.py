ce713e82976d7b800d5b898ea1de6a354a3d8490
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-05-02 18:32:45 +0200

    Increase word lengths for long float benchmarks

--- benchmark/ci_float_scalar_benchmarks.py
+++ benchmark/ci_float_scalar_benchmarks.py
@@ -11,15 +11,15 @@ def test_float_scalar_multiplication_short(benchmark):
 
 
 def test_float_scalar_multiplication_long(benchmark):
-    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_float_scalar_multiplication_mixed(benchmark):
     a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 4, 7)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x * y, a, b)
 
@@ -32,15 +32,15 @@ def test_float_scalar_division_short(benchmark):
 
 
 def test_float_scalar_division_long(benchmark):
-    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x / y, a, b)
 
 
 def test_float_scalar_division_mixed(benchmark):
     a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 4, 7)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x / y, a, b)
 
@@ -53,15 +53,15 @@ def test_float_scalar_addition_short(benchmark):
 
 
 def test_float_scalar_addition_long(benchmark):
-    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_float_scalar_addition_mixed(benchmark):
     a = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 4, 7)
-    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 50)
+    b = APyFloat.from_float(np.random.rand(1)[0] - 0.5, 16, 60)
 
     benchmark(lambda x, y: x + y, a, b)
 
@@ -75,7 +75,7 @@ def test_float_scalar_creation_short(benchmark):
 def test_float_scalar_creation_long(benchmark):
     a = np.random.rand(1)[0] - 0.5
 
-    benchmark(APyFloat.from_float, a, 16, 50)
+    benchmark(APyFloat.from_float, a, 16, 60)
 
 
 def test_float_scalar_to_float_short(benchmark):
