a94aab0f6c0385cebf41b17c46951b01f527fd87
Theodor Lindberg <theodor.lindberg@liu.se>
2024-10-17 11:45:08 +0200

    Run the cocotb example in docs, and cleanup dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ intersphinx_mapping = {
     "numpy": ("https://numpy.org/doc/stable/", None),
     "nanobind": ("https://nanobind.readthedocs.io/en/latest/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "cocotb": ("https://docs.cocotb.org/en/stable/", None),
 }
 
 # Breathe Configuration
