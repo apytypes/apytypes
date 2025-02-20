75e98a3e6c00b53fa62d4e6c63972be8f4c702d1
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-02-20 14:55:21 +0100

    Do no generate stub files for Python files

--- stubgen.sh
+++ stubgen.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 python3 -c "from nanobind import __version__ as v; print(f'Nanobind version: {v}')"
-python3 -m nanobind.stubgen -P -m apytypes -o lib/apytypes/__init__.pyi
 python3 -m nanobind.stubgen -m apytypes._apytypes -o lib/apytypes/_apytypes.pyi
-python3 -m nanobind.stubgen -m apytypes._array_functions -o lib/apytypes/_array_functions.pyi
-python3 -m nanobind.stubgen -m apytypes._utils -o lib/apytypes/_utils.pyi
-python3 -m nanobind.stubgen -m apytypes.vhdl -o lib/apytypes/vhdl.pyi
