aeb5900e11a19ff40743844b3c36d614d6ecb5cf
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-03-04 11:04:13 +0100

    Add convolve benchmarks

--- benchmark/ci_fixed_array_benchmarks.py
+++ benchmark/ci_fixed_array_benchmarks.py
@@ -153,78 +153,106 @@ def test_fixed_matrix_abs_200(benchmark):
 
 
 def test_fixed_scalar_matrix_addition_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20, 20), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20, 20) - 0.5, bits=19, int_bits=1)
     b = APyFixed.from_float(np.random.rand(1)[0], bits=19, int_bits=1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_matrix_addition_200(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(200, 200), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(200, 200) - 0.5, bits=19, int_bits=1)
     b = APyFixed.from_float(np.random.rand(1)[0], bits=19, int_bits=1)
 
     benchmark(lambda x, y: x + y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20, 20), bits=19, int_bits=1)
-    b = APyFixed.from_float(np.random.rand(1)[0], bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20, 20) - 0.5, bits=19, int_bits=1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, bits=19, int_bits=1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_200(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(200, 200), bits=19, int_bits=1)
-    b = APyFixed.from_float(np.random.rand(1)[0], bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(200, 200) - 0.5, bits=19, int_bits=1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, bits=19, int_bits=1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_medium_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20, 20), bits=40, int_bits=1)
-    b = APyFixed.from_float(np.random.rand(1)[0], bits=40, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20, 20) - 0.5, bits=40, int_bits=1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, bits=40, int_bits=1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_scalar_matrix_multiplication_medium_200(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(200, 200), bits=40, int_bits=1)
-    b = APyFixed.from_float(np.random.rand(1)[0], bits=40, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(200, 200) - 0.5, bits=40, int_bits=1)
+    b = APyFixed.from_float(np.random.rand(1)[0] - 0.5, bits=40, int_bits=1)
 
     benchmark(lambda x, y: x * y, a, b)
 
 
 def test_fixed_matrix_creation_20(benchmark):
-    a = np.random.rand(20, 20)
+    a = np.random.rand(20, 20) - 0.5
 
     benchmark(APyFixedArray.from_float, a, 1, 19)
 
 
 def test_fixed_matrix_creation_200(benchmark):
-    a = np.random.rand(200, 200)
+    a = np.random.rand(200, 200) - 0.5
 
     benchmark(APyFixedArray.from_float, a, 1, 19)
 
 
 def test_fixed_matrix_to_numpy_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20, 20), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20, 20) - 0.5, bits=19, int_bits=1)
 
     benchmark(lambda x: x.to_numpy(), a)
 
 
 def test_fixed_matrix_to_numpy_200(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(200, 200), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(200, 200) - 0.5, bits=19, int_bits=1)
 
     benchmark(lambda x: x.to_numpy(), a)
 
 
 def test_fixed_vector_sum_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=19, int_bits=1)
 
     benchmark(sum, a)
 
 
 def test_fixed_vector_max_20(benchmark):
-    a = APyFixedArray.from_float(np.random.rand(20), bits=19, int_bits=1)
+    a = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=19, int_bits=1)
 
     benchmark(max, a)
+
+
+def test_fixed_convolve_20(benchmark):
+    a = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=19, int_bits=1)
+    b = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=19, int_bits=1)
+
+    benchmark(lambda x, y: x.convolve(y), a, b)
+
+
+def test_fixed_convolve_200(benchmark):
+    a = APyFixedArray.from_float(np.random.rand(200) - 0.5, bits=19, int_bits=1)
+    b = APyFixedArray.from_float(np.random.rand(200) - 0.5, bits=19, int_bits=1)
+
+    benchmark(lambda x, y: x.convolve(y), a, b)
+
+
+def test_fixed_convolve_medium_20(benchmark):
+    a = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=39, int_bits=1)
+    b = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=39, int_bits=1)
+
+    benchmark(lambda x, y: x.convolve(y), a, b)
+
+
+def test_fixed_convolve_long_20(benchmark):
+    a = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=119, int_bits=1)
+    b = APyFixedArray.from_float(np.random.rand(20) - 0.5, bits=119, int_bits=1)
+
+    benchmark(lambda x, y: x.convolve(y), a, b)
