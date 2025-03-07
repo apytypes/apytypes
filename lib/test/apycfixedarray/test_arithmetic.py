e02945375fee66f307f483ff77137efdc637f4ee
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-03-07 13:42:58 +0100

    Add tests for varying word length for complex fixed arrays

--- lib/test/apycfixedarray/test_arithmetic.py
+++ lib/test/apycfixedarray/test_arithmetic.py
@@ -1,4 +1,6 @@
-from apytypes import APyCFixedArray, APyCFixed
+from apytypes import APyCFixedArray, APyCFixed, fx
+import math
+import pytest
 
 
 def test_prod():
@@ -33,3 +35,94 @@ def test_arithmetic_float():
             [2, 3 + 1j, 4 + 3j, -3 + 1j, 6 - 2j, 7 - 3j], int_bits=7, frac_bits=0
         )
     )
+
+
+@pytest.mark.parametrize("frac_bits", range(10, 68))
+def test_array_multiplication_with_different_frac_bits(frac_bits):
+    v1 = 1 / 3 + 1j * math.pi
+    v2 = math.pi + 1j / 7
+    v3 = -math.pi - 1j * math.sqrt(2)
+    v4 = -23 / 11 + 1j * 5 / 13
+    a = fx(v1, int_bits=3, frac_bits=frac_bits)
+    b = fx(v2, int_bits=3, frac_bits=frac_bits)
+    c = fx(v3, int_bits=3, frac_bits=frac_bits)
+    d = fx(v4, int_bits=3, frac_bits=frac_bits)
+
+    av = APyCFixedArray.from_complex([v1, v2, v3, v4], int_bits=3, frac_bits=frac_bits)
+    bv = APyCFixedArray.from_complex([v4, v1, v2, v3], int_bits=3, frac_bits=frac_bits)
+    res = av * bv
+    assert res[0].is_identical(a * d)
+    assert res[1].is_identical(b * a)
+    assert res[2].is_identical(c * b)
+    assert res[3].is_identical(d * c)
+
+
+@pytest.mark.parametrize("frac_bits", range(10, 68))
+def test_array_multiplication_with_different_frac_bits_and_int_bit_offset(frac_bits):
+    v1 = 1 / 3 + 1j * math.pi
+    v2 = math.pi + 1j / 7
+    v3 = -math.pi - 1j * math.sqrt(2)
+    v4 = -23 / 11 + 1j * 5 / 13
+    a = fx(v1, int_bits=3, frac_bits=frac_bits)
+    b = fx(v2, int_bits=3, frac_bits=frac_bits)
+    c = fx(v3, int_bits=3, frac_bits=frac_bits)
+    d = fx(v4, int_bits=3, frac_bits=frac_bits)
+
+    al = fx(v1, int_bits=3, frac_bits=frac_bits + 20)
+    bl = fx(v2, int_bits=3, frac_bits=frac_bits + 20)
+    cl = fx(v3, int_bits=3, frac_bits=frac_bits + 20)
+    dl = fx(v4, int_bits=3, frac_bits=frac_bits + 20)
+
+    av = APyCFixedArray.from_complex([v1, v2, v3, v4], int_bits=3, frac_bits=frac_bits)
+    bv = APyCFixedArray.from_complex(
+        [v4, v1, v2, v3], int_bits=3, frac_bits=frac_bits + 20
+    )
+    res = av * bv
+    assert res[0].is_identical(a * dl)
+    assert res[1].is_identical(b * al)
+    assert res[2].is_identical(c * bl)
+    assert res[3].is_identical(d * cl)
+
+
+@pytest.mark.parametrize("frac_bits", range(10, 68))
+def test_scalar_array_multiplication_with_different_frac_bits(frac_bits):
+    v1 = 1 / 3 + 1j * math.pi
+    v2 = math.pi + 1j / 7
+    v3 = -math.pi - 1j * math.sqrt(2)
+    v4 = -23 / 11 + 1j * 5 / 13
+    a = fx(v1, int_bits=3, frac_bits=frac_bits)
+    b = fx(v2, int_bits=3, frac_bits=frac_bits)
+    c = fx(v3, int_bits=3, frac_bits=frac_bits)
+    d = fx(v4, int_bits=3, frac_bits=frac_bits)
+
+    av = APyCFixedArray.from_complex([v1, v2, v3, v4], int_bits=3, frac_bits=frac_bits)
+    res = b * av
+
+    assert res[0].is_identical(b * a)
+    assert res[1].is_identical(b * b)
+    assert res[2].is_identical(b * c)
+    assert res[3].is_identical(b * d)
+
+
+@pytest.mark.parametrize("frac_bits", range(10, 68))
+def test_scalar_array_multiplication_with_different_frac_bits_and_int_bit_offset(
+    frac_bits,
+):
+    v1 = 1 / 3 + 1j * math.pi
+    v2 = math.pi + 1j / 7
+    v3 = -math.pi - 1j * math.sqrt(2)
+    v4 = -23 / 11 + 1j * 5 / 13
+    a = fx(v1, int_bits=3, frac_bits=frac_bits)
+    b = fx(v2, int_bits=3, frac_bits=frac_bits)
+    c = fx(v3, int_bits=3, frac_bits=frac_bits)
+    d = fx(v4, int_bits=3, frac_bits=frac_bits)
+
+    bl = fx(v2, int_bits=3, frac_bits=frac_bits + 20)
+
+    av = APyCFixedArray.from_complex([v1, v2, v3, v4], int_bits=3, frac_bits=frac_bits)
+    res = bl * av
+
+    assert res[0].is_identical(bl * a)
+    assert res[1].is_identical(bl * b)
+    assert res[2].is_identical(bl * c)
+    assert res[3].is_identical(bl * d)
