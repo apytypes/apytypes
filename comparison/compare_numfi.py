504b7fc7cb712fff9284c848ee038e063229d743
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-05-28 22:23:19 +0200

    Update comparisons: fix numfi and fxpmath, add en_cl_fix

--- comparison/compare_numfi.py
+++ comparison/compare_numfi.py
@@ -2,8 +2,8 @@ import numfi
 
 
 def create_scalar(val, integer_bits, fractional_bits):
-    return numfi(val, 1, integer_bits + fractional_bits, fractional_bits)
+    return numfi.numfi(val, 1, integer_bits + fractional_bits, fractional_bits)
 
 
 def create_array(val, integer_bits, fractional_bits):
-    return numfi(val, 1, integer_bits + fractional_bits, fractional_bits)
+    return numfi.numfi(val, 1, integer_bits + fractional_bits, fractional_bits)
