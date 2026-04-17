bda7408d757a3e97a14d53b5a13a218e656f72bd
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2026-04-17 12:15:26 +0200

    Add tests for mixed complex-real arithmetic with different word lengths

--- lib/test/apycfixed/test_arithmetic.py
+++ lib/test/apycfixed/test_arithmetic.py
@@ -263,3 +263,125 @@ def test_real_complex_division_different_wordlengths_different_fracbits(
         3 + 4j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
     )
     assert a / b == 3 - 4j
+
+
+@pytest.mark.parametrize("real_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("real_frac_bits", [0, 5, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_frac_bits", [0, 5, 20, 40, 100, 10000])
+def test_complex_real_multiplication_different_wordlengths_different_fracbits(
+    real_int_bits: int,
+    real_frac_bits: int,
+    complex_int_bits: int,
+    complex_frac_bits: int,
+):
+    a = APyCFixed.from_complex(
+        1 - 1j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
+    )
+    b = APyFixed.from_float(2, int_bits=real_int_bits, frac_bits=real_frac_bits)
+    res = APyCFixed.from_complex(
+        2 - 2j,
+        int_bits=complex_int_bits + real_int_bits + 1,
+        frac_bits=complex_frac_bits + real_frac_bits,
+    )
+    assert (a * b).is_identical(res)
+    assert (b * a).is_identical(res)
+    assert a * b == 2 - 2j
+    assert b * a == 2 - 2j
+
+    c = APyFixed.from_float(-2, int_bits=real_int_bits, frac_bits=real_frac_bits)
+    res = APyCFixed.from_complex(
+        -2 + 2j,
+        int_bits=complex_int_bits + real_int_bits + 1,
+        frac_bits=complex_frac_bits + real_frac_bits,
+    )
+    assert (a * c).is_identical(res)
+    assert (c * a).is_identical(res)
+    assert a * c == -2 + 2j
+    assert c * a == -2 + 2j
+
+
+@pytest.mark.parametrize("real_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("real_frac_bits", [0, 5, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_frac_bits", [0, 5, 20, 40, 100, 10000])
+def test_complex_real_addition_different_wordlengths_different_fracbits(
+    real_int_bits: int,
+    real_frac_bits: int,
+    complex_int_bits: int,
+    complex_frac_bits: int,
+):
+    a = APyCFixed.from_complex(
+        1 + 1j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
+    )
+    b = APyFixed.from_float(2, int_bits=real_int_bits, frac_bits=real_frac_bits)
+    res = APyCFixed.from_complex(
+        3 + 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (a + b).is_identical(res)
+    assert (b + a).is_identical(res)
+    assert a + b == 3 + 1j
+    assert b + a == 3 + 1j
+
+    c = APyCFixed.from_complex(
+        -1 - 1j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
+    )
+    res = APyCFixed.from_complex(
+        1 - 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (b + c).is_identical(res)
+    assert (c + b).is_identical(res)
+    assert b + c == 1 - 1j
+    assert c + b == 1 - 1j
+
+
+@pytest.mark.parametrize("real_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("real_frac_bits", [0, 5, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_int_bits", [3, 10, 20, 40, 100, 10000])
+@pytest.mark.parametrize("complex_frac_bits", [0, 5, 20, 40, 100, 10000])
+def test_complex_real_subtraction_different_wordlengths_different_fracbits(
+    real_int_bits: int,
+    real_frac_bits: int,
+    complex_int_bits: int,
+    complex_frac_bits: int,
+):
+    a = APyCFixed.from_complex(
+        1 + 1j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
+    )
+    b = APyFixed.from_float(2, int_bits=real_int_bits, frac_bits=real_frac_bits)
+    res = APyCFixed.from_complex(
+        -1 + 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (a - b).is_identical(res)
+    res = APyCFixed.from_complex(
+        1 - 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (b - a).is_identical(res)
+    assert a - b == -1 + 1j
+    assert b - a == 1 - 1j
+
+    c = APyCFixed.from_complex(
+        -1 - 1j, int_bits=complex_int_bits, frac_bits=complex_frac_bits
+    )
+    res = APyCFixed.from_complex(
+        -3 - 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (c - b).is_identical(res)
+    res = APyCFixed.from_complex(
+        3 + 1j,
+        int_bits=max(complex_int_bits, real_int_bits) + 1,
+        frac_bits=max(complex_frac_bits, real_frac_bits),
+    )
+    assert (b - c).is_identical(res)
+    assert b - c == 3 + 1j
+    assert c - b == -3 - 1j
